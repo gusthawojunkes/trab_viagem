3e27b43c4a77023049ca15e6e30a88109b66b3ba
Nathan-Stuber-Antunes <nathan.stuber.a@gmail.com>
2019-10-15 14:31:26 -0300

    Adicionei inputs e validações

--- code.js
+++ code.js
@@ -1,27 +1,32 @@
 //-----------   CONTADOR DE VISITANTES DO SESI/SENAI  ------------------
-
-var string = localStorage.getItem("Contador_1");    //capta o conteudo do localStorage
-var int = Number(string);   //converte para numeral
-$("#cont1").append(int);    //atualiza a tag h2(contador na tela)
-var cont = 0 + int; //adiciona ao contador
-
+var string1 = localStorage.getItem("Contador_1");                        //capta o conteudo do localStorage
+var int1 = Number(string1);                                               //converte para numeral
+$("#cont1").append(int1);                                                //atualiza a tag h2(contador na tela)
+var cont1 = 0 + int1;                                                     //adiciona ao contador
 
 $("#btn1").on("click",function(evt){
-    cont += 1
-    $("#cont1").empty();    //limpa a tag h2
-    $("#cont1").append(cont);    //adiona à tag h2
-    localStorage.setItem("Contador_1", cont);    //adiona ao localStorage o novo valor
+    if($("#nome1").val() == '' || $('#turmas').val() == ''){
+    }else{
+        cont1 += 1;
+        $("#cont1").empty();                                                //limpa a tag h2
+        $("#cont1").append(cont1);                                           //adiona à tag h2
+        localStorage.setItem("Contador_1", cont1);                           //adiona ao localStorage o novo valor
+    }
 });
 
 //-----------   CONTADOR  DE VISITANTES DE FORA  -----------------------
-var string1 = localStorage.getItem("Contador_2");
-var int1 = Number(string1);
-$("#cont2").append(int1);
-var cont1 = 0 + int1;
+var string2 = localStorage.getItem("Contador_2");
+var int2 = Number(string2);
+$("#cont2").append(int2);
+var cont2 = 0 + int2;
 
 $("#btn2").on("click",function(evt){
-    cont1 += 1
-    $("#cont2").empty();
-    $("#cont2").append(cont1);
-    localStorage.setItem("Contador_2", cont1);
+    if($("#nome2").val() == '' || $("#formação").val() == '' || $("#trabalho").val() == ''){
+    }else{
+        cont2 += 1;
+        $("#cont2").empty();
+        $("#cont2").append(cont2);
+        localStorage.setItem("Contador_2", cont2);
+    }
 });
+
